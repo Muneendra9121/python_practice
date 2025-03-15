8fedf0e569780b6e61ba38936cc3bc1fb20f7f23
Muneendra4321 <muneendraethireddy@gmail.com>
2025-03-15 13:32:58 +0530

    push condition flows

---
+++ conditionFlows_Loops.py
@@ -0,0 +1,11 @@
+
+my_list=[12,34,True,False,'Shiva',"Muneendra",32,67,85]
+sorted_list=my_list.sort
+# print(my_list)
+
+my_set={34,54,87,'Shivaji',"Rajesh"}
+print(my_set.discard(97)) 
+
+my_tuple=(12,78,97,43,12,78,98,48)
+i=my_tuple.count(97)
+print(i)
